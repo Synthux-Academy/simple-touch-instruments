f550023c22907480d95502da8b037939067a1087
Vladislav Litvinenko <thatvlad@icloud.com>
2023-11-18 22:16:24 +0100

    Wrap

---
+++ daisyduino/simple-drum-machine-touch/simplehh.h
@@ -0,0 +1,21 @@
+#pragma once;
+#include "DaisyDuino.h"
+
+namespace synthux {
+  class SimpleHH {
+    public:
+      void Init(float sample_rate) {
+        _hh.Init(sample_rate);
+        _hh.SetDecay(0.7);
+        _hh.SetTone(0.8);
+        _hh.SetNoisiness(0.7);
+      }
+
+      float Process(bool gate) {
+        return _hh.Process(gate);
+      }
+
+    private:
+      HiHat<> _hh;
+  };
+};
