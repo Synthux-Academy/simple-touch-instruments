621edb72c464eeeb395bb1d2cf7c91ca0daafd36
Vladislav Litvinenko <thatvlad@icloud.com>
2023-11-19 23:05:32 +0100

    Tweak sd

--- daisyduino/simple-drum-machine-touch/simplesd.h
+++ daisyduino/simple-drum-machine-touch/simplesd.h
@@ -1,3 +1,4 @@
+#include "utility/DaisySP/modules/overdrive.h"
 #pragma once
 
 #include "DaisyDuino.h"
@@ -14,7 +15,7 @@ public:
     _env.SetTime(ADSR_SEG_RELEASE, .03);
     _osc.Init(sample_rate);
     _osc.SetWaveform(Oscillator::WAVE_TRI);
-    _osc.SetFreq(100.f);
+    _osc.SetFreq(_base_freq);
   }
 
   float Process(bool trigger) {
@@ -22,17 +23,23 @@ public:
     auto amp = _env.Process(trigger);
     _noise.SetAmp(amp);
     _osc.SetAmp(amp * 0.5);
-    return _osc.Process() + _noise.Process();
+    return _drv.Process(_osc.Process() + _noise.Process() * _noise_kof);
   }
 
   void SetSound(float value) {
-    
+    _base_freq = 80.f + 420.f * value;
+    _noise_kof = (1 - value);
+    _env.SetTime(ADSR_SEG_RELEASE, .005f + (.1f * value));
+    if (value > 0.5) _drv.SetDrive(0.1 + 0.5 * value);
   }
 
 private:
   Adsr _env;
   WhiteNoise _noise;
   Oscillator _osc;
+  Overdrive _drv;
+  float _base_freq = 100.f;
+  float _noise_kof = 1.0;
 };
 
 };
