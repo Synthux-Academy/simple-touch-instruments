cabb1e6ce4b033032b0f693e2b82807f17d9cb1d
Vladislav Litvinenko <thatvlad@icloud.com>
2023-11-22 20:17:36 +0100

    Update trk.cpp

--- daisyduino/simple-drum-machine-touch/trk.cpp
+++ daisyduino/simple-drum-machine-touch/trk.cpp
@@ -19,6 +19,8 @@ bool Track::Tick() {
         if (++_slot == _pattern.size()) _slot = 0;
         if (_is_clearing) _clear(_slot);
     }
+
+    
     // As slot is changing in advance, i.e. (_slot-0.5)
     // and resolution is 2x pattern length, every second 
     // same slot value denotes onset.
