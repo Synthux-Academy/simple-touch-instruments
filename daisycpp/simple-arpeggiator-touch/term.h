ae0d1625549d2a0145f5765aa7a851702341ab80
Michael Jones <mdjones71@gmail.com>
2023-12-22 10:14:13 -0500

    Re-order to remove 'will be initialized after' warnings

--- daisycpp/simple-arpeggiator-touch/term.h
+++ daisycpp/simple-arpeggiator-touch/term.h
@@ -97,13 +97,14 @@ namespace synthux {
         _hold.fill(false);
       }
 
-      void(*_on_note_on)(uint8_t num, uint8_t vel);
-      void(*_on_note_off)(uint8_t num);
       void(*_on_scale_select)(uint8_t index);
 
       Mpr121I2C _cap;
       uint16_t _state;
       std::array<bool, kNotesCount> _hold;
       bool _latch;
+
+      void(*_on_note_on)(uint8_t num, uint8_t vel);
+      void(*_on_note_off)(uint8_t num);
   };
 };
