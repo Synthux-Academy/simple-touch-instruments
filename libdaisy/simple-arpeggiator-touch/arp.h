273324b1292abc87ec1ce8a3e228680967df7dc5
Michael Jones <mdjones71@gmail.com>
2024-01-11 19:54:01 -0500

    Renamed randomrange.h for now. Think about using https://cplusplus.com/reference/random/
    https://github.com/Synthux-Academy/simple-examples-touch/pull/1#discussion_r1442255957

--- libdaisy/simple-arpeggiator-touch/arp.h
+++ libdaisy/simple-arpeggiator-touch/arp.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../utils.h"
+#include "../randomrange.h"
 
 namespace synthux {
 
--- libdaisy/simple-arpeggiator-touch/vox.h
+++ libdaisy/simple-arpeggiator-touch/vox.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "../utils.h"
+#include "../randomrange.h"
 #include "daisysp.h"
 
 using namespace daisysp;
