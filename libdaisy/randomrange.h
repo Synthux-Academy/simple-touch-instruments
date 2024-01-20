f45c10319512e3d59dce0be3328dd0bb79cb2abf
Michael Jones <mdjones71@gmail.com>
2024-01-20 16:22:02 -0500

    Use https://cplusplus.com/reference/random/ for random numner gen

--- libdaisy/randomrange.h
+++ libdaisy/randomrange.h
@@ -7,6 +7,8 @@
  * v3 = rand() % 30 + 1985;   // v3 in the range 1985-2014 
  */ 
 int randomrange(int start, int stop){
-    int v = rand() % (stop-start) + start;
+    std::default_random_engine generator;
+    std::uniform_int_distribution<int> distribution(start,stop);
+    int v = distribution(generator);
     return v;
 }
