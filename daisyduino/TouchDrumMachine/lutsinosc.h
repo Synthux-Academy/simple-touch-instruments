071c7aa746b9b0c7d4142ad95620fb2f1ab4ac8e
Vladislav Litvinenko <thatvlad@icloud.com>
2024-06-15 13:29:57 +0200

    no message

--- daisyduino/TouchDrumMachine/lutsinosc.h
+++ daisyduino/TouchDrumMachine/lutsinosc.h
@@ -7,8 +7,8 @@ namespace synthux {
 template<size_t size>
 static constexpr std::array<float, size> sineLUT() {
     std::array<float, size> lut { 0 };
-    for (int i = 0; i < size; i++) {
-        lut[i] = sinf(2.f * M_PI * static_cast<float>(i) / static_cast<float>(size)); 
+    for (size_t i = 0; i < size; i++) {
+        lut[i] = std::clamp(sinf(2.f * M_PI * static_cast<float>(i) / static_cast<float>(size)), -1.f, 1.f); 
     }
     return lut;
   }
@@ -18,15 +18,13 @@ class LUTSinOsc {
 public:
     LUTSinOsc():
     _f_kof        { 0 },
-    _sample_rate  { 0 },
     _phase        { 0 },
-    _phase_delta  { 0 } 
+    _phase_delta  { 0 }
     {}
 
     ~LUTSinOsc() {}
 
     void Init(float sample_rate) {
-        _sample_rate = sample_rate;
         _f_kof = static_cast<float>(_lut.size()) / sample_rate;
     }
 
@@ -39,12 +37,13 @@ public:
     }
 
     float Process() {
-      auto _int_phase = static_cast<uint32_t>(_phase);
+      auto _int_phase = static_cast<size_t>(_phase);
       auto _frac_phase = _phase - _int_phase;
-      auto _next_phase = (_int_phase + 1) % _lut.size();
-      auto s = _lut[_phase] + _frac_phase * (_lut[_next_phase] - _lut[_phase]);
+      auto _next_phase = (_int_phase + 1);
+      if (_next_phase >= _lut.size()) _next_phase -= _lut.size();
+      auto s = _lut[_int_phase] + _frac_phase * (_lut[_next_phase] - _lut[_int_phase]);
       _phase += _phase_delta;
-      if (_phase > _lut.size() - 1) _phase -= (_lut.size() - 1);
+      while (_phase >= _lut.size()) _phase -= (_lut.size());
       return s;
     }
 
@@ -55,7 +54,6 @@ private:
     LUTSinOsc& operator=(LUTSinOsc &&other) = delete;
 
     float _f_kof;
-    float _sample_rate;
     float _phase;
     float _phase_delta;
     static constexpr std::array<float, size> _lut = { sineLUT<size>() };
