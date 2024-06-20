c6c10a612ae2e6e17a32ab6891c4c62b87c8cf4c
Vladislav Litvinenko <thatvlad@icloud.com>
2024-06-20 21:41:35 +0200

    no message

--- daisyduino/TouchDrumMachine/lutsinosc.h
+++ daisyduino/TouchDrumMachine/lutsinosc.h
@@ -17,9 +17,10 @@ struct LUTSin {
 class LUTSinOsc {
 public:
     LUTSinOsc():
-    _freq_kof     { 0 },
-    _phase        { 0 },
-    _phase_delta  { 0 }
+    _freq_kof         { 0 },
+    _phase            { 0 },
+    _phase_increment  { 0 },
+    _phase_offset     { 0 }
     {}
 
     ~LUTSinOsc() {}
@@ -29,7 +30,11 @@ public:
     }
 
     void SetFreq(const float freq) {
-        _phase_delta = freq * _freq_kof;
+        _phase_increment = freq * _freq_kof;
+    }
+
+    void SetPhaseOffset(const float value) {
+      _phase_offset = static_cast<float>(kSize) * value;
     }
 
     void Reset() {
@@ -37,16 +42,19 @@ public:
     }
 
     float Process() {
+      auto size = static_cast<float>(kSize);
+      auto phase = _wrap_min_max(_phase + _phase_offset, 0.f, size);
+
       //Do linear interpollation a + k * (b - a)
-      auto _int_phase = static_cast<size_t>(_phase);
-      auto _frac_phase = _phase - _int_phase;
+      auto _int_phase = static_cast<size_t>(phase);
+      auto _frac_phase = phase - _int_phase;
       auto _next_phase = _int_phase + 1;
       while (_next_phase >= kSize) _next_phase -= kSize;
       auto sample = _lut.table[_int_phase] + _frac_phase * (_lut.table[_next_phase] - _lut.table[_int_phase]);
 
       //Advance phase
-      _phase += _phase_delta;
-      while (_phase >= kSize) _phase -= kSize;
+      _phase += _phase_increment;
+      while (_phase >= size) _phase -= size;
       
       return sample;
     }
@@ -57,9 +65,18 @@ private:
     LUTSinOsc& operator=(const LUTSinOsc &other) = delete;
     LUTSinOsc& operator=(LUTSinOsc &&other) = delete;
 
+    // https://stackoverflow.com/questions/4633177/how-to-wrap-a-float-to-the-interval-pi-pi
+    float _wrap_max(const float x, const float max) {
+      return fmodf(max + fmodf(x, max), max);
+    }
+    float _wrap_min_max(const float x, const float min, const float max) {
+      return min + _wrap_max(x - min, max - min);
+    }
+
     float _freq_kof;
     float _phase;
-    float _phase_delta;
+    float _phase_increment;
+    float _phase_offset;
 
     static constexpr size_t kSize = 512;
     static constexpr LUTSin<kSize> _lut = LUTSin<kSize>();
